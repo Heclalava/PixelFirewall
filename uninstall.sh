f0260fd7df75073f79708b3e6c8fb2e0ce908261
Heclalava <drshaunhutchinson@gmail.com>
2026-09-09 11:49:34 +0800

    Add safe PixelFirewall uninstall cleanup

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,37 @@
 #!/system/bin/sh
+
 IPTABLES="/system/bin/iptables"
 IP6TABLES="/system/bin/ip6tables"
 
-# Remove all NetBlock rules
-for CMD in "$IPTABLES" "$IP6TABLES"; do
-    $CMD -F NETBLOCK 2>/dev/null
-    $CMD -D OUTPUT -j NETBLOCK 2>/dev/null
-    $CMD -X NETBLOCK 2>/dev/null
-done
+MAIN_CHAIN="PIXELFW"
+MOBILE_CHAIN="PIXELFW-MOBILE"
+WIFI_CHAIN="PIXELFW-WIFI"
+LAN_CHAIN="PIXELFW-LAN"
 
-# Remove data
-rm -rf /data/adb/netblock
-rm -rf /data/adb/modules/netblock
+remove_chain() {
+    TABLE="$1"
+    CHAIN="$2"
+
+    while "$TABLE" -C OUTPUT -j "$CHAIN" >/dev/null 2>&1; do
+        "$TABLE" -D OUTPUT -j "$CHAIN" >/dev/null 2>&1 || break
+    done
+
+    "$TABLE" -F "$CHAIN" >/dev/null 2>&1
+    "$TABLE" -X "$CHAIN" >/dev/null 2>&1
+}
+
+remove_firewall() {
+    TABLE="$1"
+
+    remove_chain "$TABLE" "$MAIN_CHAIN"
+    remove_chain "$TABLE" "$MOBILE_CHAIN"
+    remove_chain "$TABLE" "$WIFI_CHAIN"
+    remove_chain "$TABLE" "$LAN_CHAIN"
+}
+
+remove_firewall "$IPTABLES"
+remove_firewall "$IP6TABLES"
+
+rm -rf /data/adb/pixelfirewall
+
+exit 0
